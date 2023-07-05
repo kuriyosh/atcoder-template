cafb20e29df19381c691cb4bf38c391485985523
kuriyosh <yosyos0306@gmail.com>
2023-07-06 01:16:49 +0900

    git init

---
+++ tests/c.rs
@@ -0,0 +1,24 @@
+use cli_test_dir::*;
+
+const BIN: &'static str = "./c";
+
+fn test_runner(input: &str, expected: &str) {
+    let test_dir = TestDir::new(BIN, "");
+    let output = test_dir
+        .cmd()
+        .output_with_stdin(input)
+        .tee_output()
+        .expect_success();
+    assert_eq!(output.stdout_str(), expected);
+    assert!(output.stderr_str().is_empty());
+}
+
+#[test]
+fn sample1() {
+    test_runner(r"", "\n")
+}
+
+#[test]
+fn sample2() {
+    test_runner(r"", "\n")
+}
